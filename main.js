444a22ad3068096efb2d77393920445a2acbf5d4
Yooninseoung <92444452+Yooninseoung@users.noreply.github.com>
2021-11-01 21:23:25 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+//make navbar transparent when it is on the top
 const navbar=document.querySelector('#navbar');
 const navbarHeight=navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
@@ -12,3 +14,19 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('navbar--dark');
     }
 }) //scroll y좌표 알아내기
+
+
+// handle scrolling when taping on navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    
+    const target= event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }else{
+        console.log(event.target.dataset.link);
+        const scrollTo= document.querySelector(link);
+        scrollTo.scrollIntoView({behavior: 'smooth'});
+    }
+})
