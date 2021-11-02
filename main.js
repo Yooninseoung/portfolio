ba2175ca84dd6e9c1e2050b95193179a651d33a6
Yooninseoung <92444452+Yooninseoung@users.noreply.github.com>
2021-11-02 19:58:19 +0900

    JS : Transparent

--- main.js
+++ main.js
@@ -38,7 +38,29 @@ homeContactBtn.addEventListener('click', ()=>{
     scrollIntoView('#contact');
 });//클릭이 된다면
 
+
+//make home slowly fade to transparent as window scrolls down
+
+const home=document.querySelector('.home__container');
+const homeHeight=home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    console.log(window.scrollY);
+    console.log('home height : ' + homeHeight);
+
+    home.style.opacity = 1-window.scrollY/homeHeight;
+})
+
+
+
+
+
+
+
+
+//함수
 function scrollIntoView(selector){//id를 매개변수로 받아온다
     const scrollTo= document.querySelector(selector); //이동할곳의 id
     scrollTo.scrollIntoView({behavior: 'smooth'});//부드럽게 넘어가라
 }
+
+
